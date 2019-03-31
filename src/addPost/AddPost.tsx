2ebc8e9cae4d26b3f6effdcf57d99c901e848791
kendallgassner <kendall.gassner@yahoo.com>
2019-03-30 17:04:21 -0700

    prettier

--- src/addPost/AddPost.tsx
+++ src/addPost/AddPost.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../css/AddPost.css';
-import Button from "../button/button";
-import {postRequest} from "../services/Request";
+import Button from '../button/button';
+import {postRequest} from '../services/Request';
 
 const MAX_LENGTH = 140;
 const MIN_LENGTH = 10;
@@ -9,7 +9,6 @@ const MIN_LENGTH = 10;
 export interface AddPostProps {
   /** A function used to send updates to the notification banner */
   updateBanner: (message: string, isError: boolean) => void;
-
 }
 
 export interface AddPostState {
@@ -22,64 +21,63 @@ export default class AddPost extends React.Component<AddPostProps, AddPostState>
   state = {
     userId: '',
     title: '',
-    body: ''
+    body: '',
   };
 
   render() {
     const {userId, title, body} = this.state;
 
     return (
-        <div className={"AddPost"} >
+      <div className={'AddPost'}>
         <form id="form">
           <h1>Create a Post</h1>
           <div>
-            <label  htmlFor={"username"}> Username: </label>
+            <label htmlFor={'username'}> Username: </label>
             <input
-                className={"AddPost-input"}
-                type={"text"}
-                id={"username"}
-                value={userId}
-                onChange={this.usernameChange}
+              className={'AddPost-input'}
+              type={'text'}
+              id={'username'}
+              value={userId}
+              onChange={this.usernameChange}
             />
-            <label  htmlFor={"title"}> Title: </label>
+            <label htmlFor={'title'}> Title: </label>
             <input
-                className={"AddPost-input"}
-                type={"text"}
-                id={"title"}
-                value={title}
-                onChange={this.titleChange}
+              className={'AddPost-input'}
+              type={'text'}
+              id={'title'}
+              value={title}
+              onChange={this.titleChange}
             />
-            <label htmlFor={"body"}> Content: </label>
+            <label htmlFor={'body'}> Content: </label>
             <textarea
-                id={"body"}
-                className={"AddPost-textArea"}
-                value={body}
-                onChange={this.textAreaChange}
+              id={'body'}
+              className={'AddPost-textArea'}
+              value={body}
+              onChange={this.textAreaChange}
             />
           </div>
         </form>
-          <h5  className={"AddPost-limitWarning"} >{body.length}/{MAX_LENGTH}</h5>
-          <div className={"AddPost-buttonContainer"}>
-            <Button
-                onClick={this.submit}
-                label={"Submit"}
-            />
-            <Button onClick={this.close} label={"Close"}/>
-          </div>
+        <h5 className={'AddPost-limitWarning'}>
+          {body.length}/{MAX_LENGTH}
+        </h5>
+        <div className={'AddPost-buttonContainer'}>
+          <Button onClick={this.submit} label={'Submit'} />
+          <Button onClick={this.close} label={'Close'} />
         </div>
+      </div>
     );
   }
 
   private readonly usernameChange = (event: any) => {
-      this.setState({userId: event.target.value})
+    this.setState({userId: event.target.value});
   };
 
   private readonly titleChange = (event: any) => {
-    this.setState({title: event.target.value})
+    this.setState({title: event.target.value});
   };
 
   private readonly textAreaChange = (event: any) => {
-    this.setState({body: event.target.value})
+    this.setState({body: event.target.value});
   };
 
   private readonly createPostJson = () => {
@@ -102,17 +100,14 @@ export default class AddPost extends React.Component<AddPostProps, AddPostState>
     if (body.length < MIN_LENGTH || body.length > MAX_LENGTH) {
       alert(`Error character count must be >= ${MIN_LENGTH} and <= ${MAX_LENGTH}`);
       return;
-    }
-
-    else {
-      postRequest('https://jsonplaceholder.typicode.com/posts', this.createPostJson()).then(
-          successMessage  => {
-            this.props.updateBanner(successMessage as string, false);
-          })
-          .catch(errorMessage  => {
-            this.props.updateBanner(errorMessage, true);
-          });
-
+    } else {
+      postRequest('https://jsonplaceholder.typicode.com/posts', this.createPostJson())
+        .then(successMessage => {
+          this.props.updateBanner(successMessage as string, false);
+        })
+        .catch(errorMessage => {
+          this.props.updateBanner(errorMessage, true);
+        });
     }
 
     this.close();
--- src/banner/Banner.tsx
+++ src/banner/Banner.tsx
@@ -2,42 +2,35 @@ import React from 'react';
 import '../css/Banner.css';
 
 export interface ErrorProps {
-    /** The notification message to be displayed in the banner. */
-    message: string;
-    /** Determines if the notification is an error. */
-    isError: boolean;
-    /** The command to remove the banner from the screen. */
-    closeBanner: () => void;
+  /** The notification message to be displayed in the banner. */
+  message: string;
+  /** Determines if the notification is an error. */
+  isError: boolean;
+  /** The command to remove the banner from the screen. */
+  closeBanner: () => void;
 }
 
 export default class Banner extends React.Component<ErrorProps> {
+  render() {
+    const {message, closeBanner, isError} = this.props;
+    const bannerType = isError ? 'Banner-error' : 'Banner-success';
 
-    render() {
-        const {message, closeBanner, isError} = this.props;
-        const bannerType = isError? "Banner-error" : "Banner-success";
+    return (
+      <div className={['Banner', bannerType].join(' ')}>
+        <div role="alert" aria-label={message} aria-live={isError ? 'assertive' : 'polite'} />
+        <h3 className={'Banner-message'} title={message}>
+          {message}
+        </h3>
+        <button onClick={closeBanner} className={'Banner-button'} onKeyDown={this.onKeyDown}>
+          X
+        </button>
+      </div>
+    );
+  }
 
-        return (
-            <div
-                className={["Banner", bannerType].join(' ')}>
-                <div
-                    role="alert"
-                    aria-label={message}
-                    aria-live={isError? 'assertive' : 'polite'}/>
-                <h3 className={"Banner-message"} title={message}>{message}</h3>
-                <button
-                    onClick={closeBanner}
-                    className={"Banner-button"}
-                    onKeyDown={this.onKeyDown}
-
-                >X</button>
-            </div>
-        );
+  private readonly onKeyDown = (event: any) => {
+    if (event.key == 'Enter') {
+      this.props.closeBanner!();
     }
-
-    private readonly onKeyDown = (event : any) => {
-        if (event.key == 'Enter') {
-            this.props.closeBanner!();
-        }
-    }
+  };
 }
-
